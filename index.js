00cdde238f80dec35759c66faa00ab4d475d065c
Amanda R <abrolon87@gmail.com>
2019-08-13 19:01:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten(Tiger) {
+  kittens.push(Tiger)
+  return kittens;
+}
+//passed
+function destructivelyPrependKitten(Bob) {
+  kittens.unshift(Bob)
+  return kittens;
+}
+//passed
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+//passed
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+//passed
+
+//continue from here
+function appendKitten(Broom) {
+  kittens.concat(Broom)
+  return kittens;
+}
